ac0852063d48fac6669b27c62e4d38f43a40d4ec
danutbradescu <dumitru.bradescu01@e-uvt.ro>
2023-07-27 22:56:27 +0300

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { Section1Component } from './section1/section1.component';
+import { SkillsComponent } from './skills/skills.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ContactComponent,
+    ProjectsComponent,
+    Section1Component,
+    SkillsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
